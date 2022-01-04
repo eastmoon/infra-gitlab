9777b9b23ba74fe5e87cd55e69fcce1aec1574c8
Jacky Eastmoon <jacky_eastmoon@hotmail.com>
2022-01-04 17:51:27 +0800

    refactor: clear mark code.

--- shell/1-initial.sh
+++ shell/1-initial.sh
@@ -13,17 +13,17 @@ echo-i $(gitlab-version)
 
 # create group
 echo-i "Create Group"
-#create-group RD "This group for development repository."
-#create-group PM "This group for project management repository."
-#create-group QA "This group for quality assurance repository."
+create-group RD "This group for development repository."
+create-group PM "This group for project management repository."
+create-group QA "This group for quality assurance repository."
 retrieve-group
 
 # create user
 echo-i "Create User"
-#create-user testA testA@testmail.com
-#create-user testB testB@testmail.com
-#create-user testC testC@testmail.com
-#create-user testD testD@testmail.com
+create-user testA testA@testmail.com
+create-user testB testB@testmail.com
+create-user testC testC@testmail.com
+create-user testD testD@testmail.com
 retrieve-user
 
 echo-i "Add User into Group"
@@ -36,12 +36,12 @@ add-group testD QA 20
 
 # create project
 echo-i "Create Project"
-#create-project-with-readme ws-core RD
-#create-project-with-readme ws-corex RD
-#create-project-with-readme ws-corexx RD
-#create-project-with-readme ws-corexxX RD
+create-project-with-readme ws-core RD
+create-project-with-readme ws-corex RD
+create-project-with-readme ws-corexx RD
+create-project-with-readme ws-corexxX RD
 retrieve-project
-#edit-project-desc ws-core "It is a ws-core project"
-#edit-project-desc ws-corex "It is a ws-corex project"
-#edit-project-desc ws-corexx "It is a ws-corexx project"
-#edit-project-desc ws-corexxX "It is a ws-corexxX project"
+edit-project-desc ws-core "It is a ws-core project"
+edit-project-desc ws-corex "It is a ws-corex project"
+edit-project-desc ws-corexx "It is a ws-corexx project"
+edit-project-desc ws-corexxX "It is a ws-corexxX project"
