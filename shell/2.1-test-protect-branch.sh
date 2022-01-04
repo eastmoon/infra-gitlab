62ac9ebd10574190be8c819bcef17cf98705b57d
Jacky Eastmoon <jacky_eastmoon@hotmail.com>
2022-01-04 19:02:49 +0800

    feat: add protect branch function

---
+++ shell/2.1-test-protect-branch.sh
@@ -0,0 +1,24 @@
+## Gitlab 的專案在 CI 佈局的設置，建立開發主線 ( master，unprotect )、產品主線 ( release，protect )
+#!/bin/bash
+set -e
+
+# Declare variable
+source ./src/conf.sh
+# Include library
+source ./src/utils.sh
+source ./src/gitlab.sh
+
+# Execute script
+## Retrieve gitlab version infromation
+echo-i $(gitlab-version)
+
+## Setting protect branch
+retrieve-project
+unprotect-branch ws-core master
+unprotect-branch ws-corex master
+unprotect-branch ws-corexx master
+protect-branch ws-corex release*
+protect-branch ws-corexx master
+echo-i $(protect-branch-info ws-core)
+echo-i $(protect-branch-info ws-corex)
+echo-i $(protect-branch-info ws-corexx)
