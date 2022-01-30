af26d05db2812b0b79bdfc694dd304674c05214f
Jacky Eastmoon <jacky_eastmoon@hotmail.com>
2022-01-30 14:58:51 +0800

    feat: add 3-1 test protect & unportect function

---
+++ shell/3.1-test-protect-branch.sh
@@ -0,0 +1,55 @@
+## Gitlab 專案對於分支保護 ( Protect Branch ) 設定
+#!/bin/bash
+set -e
+# Include library
+source ./src/utils.sh
+source ./src/gitlab.sh
+source ./src/git.sh
+
+# Declare variable
+TEST_REPO_NAME_1=test-case-3.1-unprotect-master-`date "+%Y%m%d%H%M%S"`
+TEST_REPO_NAME_2=test-case-3.1-protect-release-`date "+%Y%m%d%H%M%S"`
+TEST_REPO_NAME_3=test-case-3.1-protect-white-card-`date "+%Y%m%d%H%M%S"`
+
+# Execute script
+## Create test repository
+create-project-with-readme ${TEST_REPO_NAME_1} QA
+create-project-with-readme ${TEST_REPO_NAME_2} QA
+create-project-with-readme ${TEST_REPO_NAME_3} QA
+retrieve-project
+
+## Clone repository
+rm -rf ${SHELL_GIT_DIR}/test-case-3.1-*
+
+## Unprotect master
+### Setting unprotect
+unprotect-branch ${TEST_REPO_NAME_1} master
+
+## Unprotect master and protect release
+### Initial repository
+git-init QA ${TEST_REPO_NAME_2} master
+### Create release branch
+git-init-branch ${TEST_REPO_NAME_2} release
+### Setting unprotect branch
+unprotect-branch ${TEST_REPO_NAME_2} master
+### Setting protect branch
+protect-branch ${TEST_REPO_NAME_2} release
+
+## Unprotect master and protect white card
+### Initial repository
+git-init QA ${TEST_REPO_NAME_3} master
+### Setting unprotect branch
+unprotect-branch ${TEST_REPO_NAME_3} master
+### Setting protect branch
+protect-branch ${TEST_REPO_NAME_3} release*
+### Create release branch
+git-init-branch ${TEST_REPO_NAME_3} release
+git-init-branch ${TEST_REPO_NAME_3} release-1.0.0.0
+
+## Show protected information
+echo-i "> Show repository ${TEST_REPO_NAME_1} protect branch information"
+echo-i $(protect-branch-info ${TEST_REPO_NAME_1})
+echo-i "> Show repository ${TEST_REPO_NAME_2} protect branch information"
+echo-i $(protect-branch-info ${TEST_REPO_NAME_2})
+echo-i "> Show repository ${TEST_REPO_NAME_3} protect branch information"
+echo-i $(protect-branch-info ${TEST_REPO_NAME_3})
