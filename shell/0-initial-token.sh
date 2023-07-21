9b529f33332d1b71afa2aeacd669b9acd3486d07
Jacky Eastmoon <jacky_eastmoon@hotmail.com>
2023-07-21 14:18:13 +0800

    fix: expires at can't be blank

--- shell/0-initial-token.sh
+++ shell/0-initial-token.sh
@@ -11,7 +11,7 @@ source ./src/conf.sh
 #gitlab-rails runner "PersonalAccessToken.find_by_token('token-string-here123').revoke!"
 ##
 echo-i "Create Personal Access Token ${GIT_ACCESS_NAME} : ${GIT_ACCESS_TOKEN}"
-CMD="token = User.find_by_username('root').personal_access_tokens.create(scopes: [:api, :read_user, :read_api, :read_repository, :write_repository, :sudo], name: '${GIT_ACCESS_NAME}'); "
+CMD="token = User.find_by_username('root').personal_access_tokens.create(scopes: [:api, :read_user, :read_api, :read_repository, :write_repository, :sudo], name: '${GIT_ACCESS_NAME}', expires_at: 365.days.from_now); "
 CMD="${CMD} token.set_token('${GIT_ACCESS_TOKEN}');"
 CMD="${CMD} token.save!"
 gitlab-rails runner "${CMD}"
